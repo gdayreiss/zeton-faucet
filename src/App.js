82bb57a682f5bd33b0d76483953691ce46fbd9ca
George Day-Reiss <20464730+gdayreiss@users.noreply.github.com>
2023-02-06 20:44:37 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
-import './App.css';
 import React, { Component } from 'react';
+import './App.css';
+import FormComponent from './FormComponent';
 
-class FormComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blockchainId: '',
-      message: ''
-    };
-  }
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const data = {
-        "senderCurrency": "SILVER",
-        "receiverCurrency": "SILVER",
-        "senderId": "0xb85973a890991e1d3cc2f5925302a532a9d17b71",
-        "senderCredentials": "0xdfbe3f4ce2c85e26a6fa9b0481c7c3ddfb8ca15e502a3d67422a5d67d5e0a89a",
-        "receiverId": this.state.blockchainId,
-        "currencyAmount": 500000
-    }
-    fetch("https://pdsapi.dase.io:8081/api/balances/transfer", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            this.setState({ message: "Funds transferred (500k Leos on PDSAPI)" });
-        } else {
-            this.setState({ message: "Error: " + response.status + " " + response.statusText});
-        }
-    })
-    .catch(error => {
-        this.setState({ message: error });
-    });
-  }
+class App extends Component {
   render() {
-    document.title = "GDR Żetonium Faucet";
     return (
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-      <img src={require('./headerimage.jpg')} width="400" height="400" alt="header image" />
-      <h1> Żetonium Faucet</h1>
-      <form onSubmit={this.handleSubmit} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <label>Żetonium ID</label>
-        <input type="text" name="blockchainId" value={this.state.blockchainId} onChange={this.handleChange} style={{margin:'10px 0'}} />
-        <button type="submit" style={{margin:'10px 0'}}>Submit</button>
-        <p>{this.state.message}</p>
-      </form>
-    </div>
+      <div className="App">
+        <FormComponent
+          endpoint="https://pdsapi.dase.io:8081/api/balances/transfer"
+          buttonText="Submit PDSAPI"
+        />
+        <FormComponent
+          endpoint="https://beta.dase.io:8081/api/balances/transfer"
+          buttonText="Submit Beta"
+        />
+      </div>
     );
   }
 }
 
-export default FormComponent;
+export default App;
