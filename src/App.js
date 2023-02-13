85749f054c29ac3f5aeb874e0d8118cee27c3556
George Day-Reiss <20464730+gdayreiss@users.noreply.github.com>
2023-02-13 18:51:41 -0500

    Fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import BalanceChecker from './BalanceChecker';
 import FormComponent from './FormComponent';
+import RequestTable from './RequestTable';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Żetonium Faucet </h1>
-        <img src={require('./headerimage.jpg')} width="400" height="400" alt="header image" />
+        <img src={require('./headerimage.jpg')} width="100" height="100" alt="header image" />
         <FormComponent
           endpoint="https://pdsapi.dase.io:8081/api/balances/transfer"
           buttonText="Submit PDSAPI"
@@ -18,6 +20,9 @@ class App extends Component {
           buttonText="Submit Beta"
           title="Beta"
         />
+        <h1>Faucet Balance</h1>
+        <BalanceChecker />
+        <RequestTable />
       </div>
     );
   }
